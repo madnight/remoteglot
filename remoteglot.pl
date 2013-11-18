a01798debae3ac16607e44f4a8fc32668bf21147
Steinar H. Gunderson <sgunderson@bigfoot.com>
2013-11-18 02:03:10 +0100

    Update slightly more often.

--- remoteglot.pl
+++ remoteglot.pl
@@ -25,7 +25,7 @@ my $engine2_cmdline = "./stockfish_13111119_x64_modern_sse42";
 my $telltarget = undef;   # undef to be silent
 my @tell_intervals = (5, 20, 60, 120, 240, 480, 960);  # after each move
 my $uci_assume_full_compliance = 0;                    # dangerous :-)
-my $update_max_interval = 2.0;
+my $update_max_interval = 1.0;
 my $second_engine_start_depth = 8;
 my @masters = (
 	'Sesse',
