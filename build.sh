5243478f883e4997134e3abc8da57b7680129c89
Steinar H. Gunderson <sgunderson@bigfoot.com>
2014-11-24 21:17:50 +0100

    Clean up the minification stuff a bit; still not really in use.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# Download http://dl.google.com/closure-compiler/compiler-latest.zip
+# and unzip it in closure/ before running this script.
+
+java -jar closure/compiler.jar \
+	--language_in ECMASCRIPT5 \
+	--compilation_level SIMPLE \
+	--js_output_file=www/js/remoteglot.min.js \
+	--externs externs/jquery-1.9.js \
+	--externs externs/webstorage.js \
+	www/js/chessboard-0.3.0.js \
+	www/js/chess.js \
+	www/js/remoteglot.js
+
