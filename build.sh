b76cf35e0e41f59faa3547652c060711ac6cf9fd
Steinar H. Gunderson <sgunderson@bigfoot.com>
2014-11-25 18:15:43 +0100

    Fetch a custom build of jQuery and compile that in; now we actually seem to make some size progress by compilation.

--- build.sh
+++ build.sh
@@ -3,12 +3,17 @@
 # Download http://dl.google.com/closure-compiler/compiler-latest.zip
 # and unzip it in closure/ before running this script.
 
+# The JQuery build comes from http://projects.jga.me/jquery-builder/,
+# more specifically
+#
+# https://raw.githubusercontent.com/jgallen23/jquery-builder/0.7.0/dist/2.1.1/jquery-deprecated-sizzle.js
+
 java -jar closure/compiler.jar \
 	--language_in ECMASCRIPT5 \
 	--compilation_level SIMPLE \
 	--js_output_file=www/js/remoteglot.min.js \
-	--externs externs/jquery-1.9.js \
 	--externs externs/webstorage.js \
+        www/js/jquery-deprecated-sizzle.js \
 	www/js/chessboard-0.3.0.js \
 	www/js/chess.js \
 	www/js/json_delta.js \
