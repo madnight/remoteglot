d40118a8adcb0da790ab1087296287e7eeab7c2c
Steinar H. Gunderson <sgunderson@bigfoot.com>
2014-12-03 18:44:15 +0100

    Remove en passant hack for Rybka 2, which is now obsolete; we would rather have the canonically correct FEN.

--- Position.pm
+++ Position.pm
@@ -75,27 +75,12 @@ sub fen {
 	my $ep = "-";
 	if ($pos->{'ep_file_num'} != -1) {
 		my $col = $pos->{'ep_file_num'};
-		my $nep = (qw(a b c d e f g h))[$col];
+		$ep = (qw(a b c d e f g h))[$col];
 
 		if ($pos->{'toplay'} eq 'B') {
-			$nep .= "3";
+			$ep .= "3";
 		} else {
-			$nep .= "6";
-		}
-
-		#
-		# Showing the en passant square when actually no capture can be made
-		# seems to confuse at least Rybka. Thus, check if there's actually
-		# a pawn of the opposite side that can do the en passant move, and if
-		# not, just lie -- it doesn't matter anyway. I'm unsure what's the
-		# "right" thing as per the standard, though.
-		#
-		if ($pos->{'toplay'} eq 'B') {
-			$ep = $nep if ($col > 0 && $pos->{'board'}[4][$col-1] eq 'p');
-			$ep = $nep if ($col < 7 && $pos->{'board'}[4][$col+1] eq 'p');
-		} else {
-			$ep = $nep if ($col > 0 && $pos->{'board'}[3][$col-1] eq 'P');
-			$ep = $nep if ($col < 7 && $pos->{'board'}[3][$col+1] eq 'P');
+			$ep .= "6";
 		}
 	}
 	$fen .= " ";
